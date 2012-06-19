8443362ae58d4fc7eb8004d5f10021977c4bedf3
Gump <michal@michal.tv>
2012-06-19 23:44:17 +0100

    Removed template paths as they're obsolete

--- rss2rest/settings.py
+++ rss2rest/settings.py
@@ -105,7 +105,7 @@ ROOT_URLCONF = 'urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'wsgi.application'
 
-TEMPLATE_DIRS = ('/Users/mszczesny/Workspace/rss2rest/rss2rest/templates',)
+TEMPLATE_DIRS = ('',)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
